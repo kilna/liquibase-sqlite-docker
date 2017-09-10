2b88235d783a7897ac81e63dfcb315e1d5593cc9
Kilna <kilna@kilna.com>
2017-09-09 19:44:49 -0500

    Updating .env with version 3.20.0

--- build/build_test_tag.sh
+++ build/build_test_tag.sh
@@ -2,21 +2,27 @@
 set -e -o pipefail
 
 project="liquibase-sqlite"
-dockerhub_user="kilna"
+driver_pretty="SQLite JDBC Driver"
+github_user="kilna"
+
+dockerhub_user=$(docker info | grep Username | cut -d ' ' -f 2)
 latest_version=$(tail -1 build/versions.txt)
 version="${1:-$latest_version}"
 
+git fetch -p origin
+[[ "$dockerhub_user" == '' ]] && echo "Must be logged into dockerhub with docker login" >&2 && exit 180
+[[ "$github_token" == '' ]] && echo "Environment variable github_token must be set" >&2 && exit 181
+
 header() { printf '=%.0s' {1..79}; echo; echo $@; printf '=%.0s' {1..79}; echo; }
 
-echo "jdbc_driver_version=$version" > .env
-
 header "Testing $project $version"
+echo "jdbc_driver_version=$version" > .env
 docker-compose -f docker-compose.test.yml down --rmi all &>/dev/null || true
-docker-compose -f docker-compose.test.yml up --exit-code-from sut --build --build-arg jdbc_driver_version="$version" --force-recreate --remove-orphans
+docker-compose -f docker-compose.test.yml up --exit-code-from sut --build --force-recreate --remove-orphans
 docker-compose -f docker-compose.test.yml down --rmi all
 
 header "Building $project $version"
-docker build --tag "$project:build" --build-arg jdbc_driver_version="$version" .
+docker build --tag "$project:build-$version" --build-arg jdbc_driver_version="$version" .
 
 header "Git Tagging $project $version"
 git fetch -p origin
@@ -25,17 +31,22 @@ git pull
 if [[ `git tag | grep -F "v$version"` != '' ]]; then
   git tag -d "v$version" || true
 fi
-git tag "v$version"
+git tag -m "From $driver_pretty $version" "v$version"
 git push origin ":v$version" || git push origin "v$version"
 git checkout .env
 
+header "GitHub Releasing $project $version"
+curl -X DELETE https://api.github.com/repos/$github_user/${project}-docker/releases/v$version?access_token=$github_token || true
+curl --data '{"tag_name": "v'"$version"'","target_commitish": "master","name": "From '"$driver_pretty"' v'"$version"'","draft": false,"prerelease": false}' \
+  https://api.github.com/repos/$github_user/${project}-docker/releases?access_token=$github_token
+
 header "Docker Tagging and Pushing $project:$version"
-docker tag "$project:build" "$dockerhub_user/$project:$version"
+docker tag "$project:build-$version" "$dockerhub_user/$project:$version"
 docker push "$dockerhub_user/$project:$version"
 
 if [[ "$version" == "$latest_version" ]]; then
   header "Docker Tagging and Pushing $project:latest"
-  docker tag "$project:build" "$dockerhub_user/$project:latest"
+  docker tag "$project:build-$version" "$dockerhub_user/$project:latest"
   docker push "$dockerhub_user/$project:latest"
 fi
 
